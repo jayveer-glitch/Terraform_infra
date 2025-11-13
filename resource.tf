5f32c3a61a89653994ab3bc67979919586e2697e
jayveer-glitch <jayveersathvara744@gmail.com>
2025-11-13 18:05:55 +0530

    error solved

--- resource.tf
+++ resource.tf
@@ -1,10 +1,8 @@
-# 1. Create a Security Group (Firewall)
 resource "aws_security_group" "web_sg" {
   name        = "web-server-sg"
   description = "Allow HTTP traffic"
-  vpc_id     = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
-  # This is the rule you will test
   ingress {
     description = "Allow HTTP traffic from anywhere"
     from_port   = var.server_http_port
@@ -13,38 +11,35 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outbound traffic
   egress {
-  description = "Allow all outbound traffic"
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# 2. Create your EC2 Instance (with two lines added)
 resource "aws_instance" "example" {
-  instance_type = "t2.micro"
-  ami           = "ami-02b8269d5e85954ef" # Your specified AMI
+  instance_type               = "t2.micro"
+  ami                         = "ami-02b8269d5e85954ef"
+  associate_public_ip_address = true
+
   root_block_device {
-    encrypted = true
+    encrypted   = true
     volume_size = 8
     volume_type = "gp2"
-      } 
-  
-  # --- THIS IS THE FIX ---
-  # We must tell the instance which network to live in
+  }
 
   subnet_id = aws_subnet.main.id
-  
-  # We must tell the instance which firewall to use
+
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-    metadata_options {
-    http_tokens = "required"
+
+  metadata_options {
+    http_tokens   = "required"
     http_endpoint = "enabled"
   }
-  # -----------------------
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World!" > index.html
@@ -54,8 +49,7 @@ resource "aws_instance" "example" {
     Name = "ExampleInstance"
   }
 }
-# 1. Create the VPC (your private network)
-# tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
@@ -64,23 +58,17 @@ resource "aws_vpc" "main" {
   }
 }
 
-# 2. Create a Subnet (a section of your network)
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  
-  # This makes it a "public" subnet
-  map_public_ip_on_launch = false 
-
-  # This is the fix for the "t2.micro not supported" error
-  availability_zone = "ap-south-1a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
 
   tags = {
     Name = "main-public-subnet"
   }
 }
 
-# 3. Create an Internet Gateway (to connect to the internet)
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 
@@ -89,12 +77,11 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-# 4. Create a Route Table (exit signs for the internet)
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0" # "Anywhere on the internet"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
@@ -103,11 +90,11 @@ resource "aws_route_table" "rt" {
   }
 }
 
-# 5. Associate the Subnet with the Route Table
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.rt.id
 }
+
 output "instance_public_ip" {
   description = "Public IP address of the web server."
   value       = aws_instance.example.public_ip
