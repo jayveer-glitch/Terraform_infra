b5a94151ba2459d2c4c27798a8688d345cbeabe4
jayveer-glitch <jayveersathvara744@gmail.com>
2025-11-18 12:05:46 +0530

    correcting workspace and test directories

--- test/main.test.go
+++ test/main.test.go
@@ -5,19 +5,27 @@ import (
 	"testing"
 	"time"
 
-	"github.com/gruntwork-io/terratest/modules/http-helper"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformAwsExample(t *testing.T) {
 	t.Parallel()
+	
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../",
+		BackendConfig: map[string]interface{}{
+			"bucket":         "demo-bucket-batak-420",
+			"key":            "path/to/my/terraform.tfstate",
+			"region":         "ap-south-1",
+			"dynamodb_table": "demo-table-batak-420",
+		},
 	}
-	defer terraform.Destroy(t, terraformOptions)
-	terraform.InitAndApply(t, terraformOptions)
+
+	//defer terraform.Destroy(t, terraformOptions)
+	//terraform.InitAndApply(t, terraformOptions)
 
 	serverPort := terraform.Output(t, terraformOptions, "server_port")
 	expectedPort := "80"
@@ -30,7 +38,7 @@ func TestTerraformAwsExample(t *testing.T) {
 
 	url := fmt.Sprintf("http://%s:%s", publicIP, serverPort)
 
-	http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, World", 30, 10*time.Second)
+	http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, World!", 30, 10*time.Second)
 
 	assert.True(t, len(publicIP) > 0, "Public IP should not be empty")
 }
