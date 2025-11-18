04feeaf3d4ebd3f5a8c6d59804af3cc5b6549d6c
jayveer-glitch <jayveersathvara744@gmail.com>
2025-11-18 12:38:21 +0530

    now it will allow traffic from anywhere

--- resource.tf
+++ resource.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = var.server_http_port
     to_port     = var.server_http_port
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
