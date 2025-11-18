baa2938dffc965125546324b8f818311807b45e7
jayveer-glitch <jayveersathvara744@gmail.com>
2025-11-18 12:46:55 +0530

    ignoring

--- resource.tf
+++ resource.tf
@@ -1,4 +1,4 @@
-# tfsec:ignore:aws-ec2-no-public-ingress-sgroup
+# tfsec:ignore:aws-ec2-no-public-ingress-sgr
 resource "aws_security_group" "web_sg" {
   name        = "web-server-sg"
   description = "Allow HTTP traffic"
